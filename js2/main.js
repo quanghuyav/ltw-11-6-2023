e368471011e23c20966543485ca24727e23972ca
quanghuyav <huyav1999@gmail.com>
2023-07-13 21:29:46 +0700

    z

---
+++ js2/main.js
@@ -0,0 +1,56 @@
+// var x = 1*2
+// var y = 1/2 + 1/0 - 3**4 + x**2 - x%"10"
+// // var x = 1 + 1 + "2" + 3 // x là biểu thức trả về giá trị là 1 => y sẽ được gán giá trị là 1
+// // // "11" + 2 +3 >> "112" +3 >> "1123"
+// // // "11" +2 + 3
+// // // 2 + "2" +3 >> "22" + 3 >> 223
+ 
+// console.log("x=",x, "kiểu dữ liệu là:",typeof x)
+
+// // number
+// 1
+// 2.5
+// -1
+// -1.7
+// // String
+// ""
+// '' 
+// ``
+// "kjhkjgh"
+// "a"
+// "1"
+// "''"
+// '"abv"' 
+// "true"
+// // Boolean
+// true
+// false
+// // underfined
+// undefined
+// // null
+// null;
+// // object
+// []  //  array
+// {}  //  object
+// // function
+// function a(){} // hàm
+
+
+// x = true
+// y = false
+
+// console.log(true&&false&&true)
+
+// console.log(false||false||false)
+
+// console.log(!true)
+
+// var name = "huy"
+// var str = `tên tôi là ${name}`
+// console.log(str)
+
+let x = 1
+ x = x + "" // string
+ x = x - 0  // number
+ x = !!x    // boolean 
+console.log(typeof x)
---
+++ js2/noidung.js
@@ -0,0 +1,137 @@
+/** 1. BIẾN TRONG JAVASCRIPT
+ *  - Trong JavaScript, biến được sử dụng để lưu trữ dữ liệu và cho phép chúng ta thực hiện các tính toán hoặc thao tác trên dữ liệu đó
+ *  - Từ khoá : var, let, const
+ *  - vd:   var x = 1
+ *          let y = x + 1
+ *          const PI = 3,14 >> HẰNG SỐ
+ */
+
+//  var(let,const) [tên biến] = [biểu thức] // biểu thức là những đoạn code mà trả về giá trị
+
+var x = 1;
+var X = 1;
+let y;
+const PI = 3.14; 
+const c = 3*(10**8)
+var x1$
+let y_2
+const ____z_____
+const name = "huy"
+const oidf = "huy"  // đặt tên không ý ngihax
+const dayLaMotCaiTenRatLaHayDoChinhLa = "huy"  // dài dòng
+/** 2. QUY TẮC ĐẶT TÊN (để đặt cho biến, hàm, đối tượng... hoặc bất cứ cái gì cần đặt tên)
+ *  - Phân biệt chữ hoa, chữ thường. Nghĩa là fullName và FullName là 2 tên khác nhau
+ *  - Chỉ sử dụng các ký tự sau khi đặt tên: các chữ cái, chữ số, dầu gạch dưới (_)
+ *  - Javascript ko ưa kí tự (-), nên là đừng dùng nó để đặt tên
+ *  - Bắt đầu bằng chữ cái hoặc dấu gạch dưới (_), không được bắt đầu bằng số hoặc ký tự đặc biệt
+ *  - Đặt tên có ý nghĩa, ngắn gọn 
+ *  - Không đặt trùng các từ khoá đã được định nghĩa sắn trong javascript (vd: var, let, function...)
+ *  - Các kiểu đặt tên
+ *      + Camel case: firstName, lastName, numberOfStudents, isNumber, person
+ *      + Pascal case: Person, LastName
+ *      + Snake case: first_name, LAST_NAME
+ */
+
+var fullName = "John Smith"
+var age = 30
+var isStudent = true
+
+var a = "John Smith"; // Tên biến quá ngắn và không nói lên ý nghĩa của biến
+var 2ndAddress = "123 Main St"; // Tên biến bắt đầu bằng số
+var this_is_my_variable_name = "test"; // Tên biến quá dài và khó đọc
+var let = "test"; // Tên biến trùng với từ khóa trong JavaScript
+var myvariabl/.e = "test"; // Tên biến sử dụng ký tự đặc biệt
+
+
+/** 3. COMMENT TRONG JS
+ *  - Mục đích:
+ *      + Ghi chú
+ *      + Vô hiệu hoá mã
+ *  - Phím tắt: Ctrl + /  (máy mac: Command + /), hoặc dùng dấu //
+ */
+
+// comtsldkmg;lsemg;lemgsdmg
+// 
+
+/*
+Đoạn mình muốn cmt
+àlmlamsf
+âo;ừpoawf
+ăkfjaspfja;leg
+*/
+
+// álkjaosf
+// aklwjfkawlf
+
+
+/** 
+ * se;kgjlksdjg
+ * sd;dkgnisegn
+ * se;lkgm;lsge
+ * lknsdglm
+ * sgosehg
+*/
+
+
+
+
+
+
+/**. KIỂU DỮ LIỆU
+ *  - Number: 1 2 3 3,14 -3
+ *  - String: "1" ; 'abc' ; "con mèo" ; "" ; "   " ; "true"
+ *  - Boolean: true/false
+ *  - Undefined: Biểu diễn biến không được gán giá trị
+ *  - Null: Biểu diễn giá trị null, tức là không có giá trị
+ *  - Object (Đối tượng) / Array (Mảng) : Object
+ *  - Function
+ *  - Ngoài ra, trong JavaScript còn có một kiểu dữ liệu đặc biệt là kiểu Symbol, được sử dụng để tạo ra các giá trị duy nhất không thể thay đổi. Tuy nhiên, kiểu này ít được sử dụng trong lập trình JavaScript cơ bản.
+ * 
+ */
+
+/** 6. TOÁN TỬ SỐ HỌC >> Biểu thức (tham số quy về number hoặc đôi khi là string) trả về 1 số (đôi khi là chuỗi)
+ *  +   --> Phép cộng
+ *  -   --> Phép trừ  (chuỗi chữ >> lỗi, chuối số >> số, true >> 1 , false >> 0, null >>0; underfined >> lỗi)
+ *  *   --> Phép nhân
+ *  /   --> Phép chia
+ *  **  --> Phép luỹ thừa
+ *  %   --> Phép chia lấy dư // 10 % 3 == 1
+ *  ++  --> Phép tăng 1 đơn vị (dùng cho biến)
+ *  --  --> Phép giảm 1 đơn vị (dùng cho biến)
+ */
+
+/** 7. TOÁN TỬ SO SÁNH >> Biểu thức (tham số quy về number) trả về 1 giá trị có kiểu boolean
+ *  ==  --> So sánh bằng, chỉ so sánh giá trị
+ *  === --> So sánh bằng, so sánh cả kiểu dữ liệu lẫn giá trị
+ *  !=  --> So sánh khác, chỉ so sánh giá trị
+ *  !== --> So sánh khác, so sánh cả kiểu dữ liệu lẫn giá trị
+ *  >   --> So sánh lớn hơn
+ *  >=  --> So sánh lớn hơn hoặc bằng
+ *  <   --> So sánh nhỏ hơn
+ *  <=  --> So sánh nhỏ hơn hoặc bằng
+ */
+
+/** 8. TOÁN TỬ LOGIC >> biểu thức (những tham số quy về boolean) trả về boolean
+ *  && --> Toán tử AND >>> vế 1 and vế 2 and vế 3 
+ *  || --> Toán tử OR   >>> vế 1 or vế 2 or vế 3
+ *  !  --> Toán tử NOT
+ */
+
+
+/** TOÁN TỬ GÁN
+ *  x = y
+ * x +=y >> x = x + y // x+=1  x = x+ 1
+ * x -=y >> x = x - y
+ * x *= >> x = x*y
+ * x /= >> x = x / y
+ * x **= >> x = x ** y
+ * x %= >> x = x % y
+ */
+
+
+// Dấu $ trong string \\\\
+
+/** CHUYỂN ĐỔI GIỮA CÁC KIỂU DỮ LIỆU
+ *
+ */
+
