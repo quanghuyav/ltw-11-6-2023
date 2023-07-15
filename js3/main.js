9dc2390b09b6f1478ddb8af2999a5e254727beb0
quanghuyav <huyav1999@gmail.com>
2023-07-15 21:14:34 +0700

    z

---
+++ js3/main.js
@@ -0,0 +1,179 @@
+// const x = 1395832
+// console.log(1)
+// if ((x%2==0) && (x%5==0)) {
+//     console.log("x chia hết cho 10")
+    
+// }
+
+// console.log(3)
+
+// const age = 12
+// const name = "abv"
+// console.log(`xin chào ${name}, năm nay ${name} ${age} tuổi`)
+// if (age > 18) {
+//     console.log("đủ tuổi trưởng thành")
+// }
+
+// let name = "huy"
+
+// if (name) {
+//     console.log("xin chào" , name)
+// }
+
+// if (false) {
+//     console.log("thực hiện câu lệnh if")
+// }
+// else {
+//     console.log("thực hiện câu lệnh else")
+// }
+
+// let age =19 
+// if (age >= 18) {
+//     console.log ("thanh niên")
+// } else
+// {
+//     console.log("thiếu nhi")
+// }
+
+// if (false) {
+//     console.log("IF")
+// } else if (false) {
+//     console.log ("else 1")
+// } else if (false) {
+//     console.log ("else 2")
+// } else {
+//     console.log("else 3")
+// }
+
+// if (true) console.log("hello")
+// if (false) console.log("abc")
+// else console.log ("xyz")
+
+// const month = 2
+// switch (month){
+//     case 1:
+//         console.log("có 31 ngày")
+//         break
+        
+//     case 2:
+//         console.log("có 28 ngày")
+//         break
+        
+//     case 3:
+//         console.log("có 31 ngày")
+//         break
+        
+//     default:
+//         console.log("làm gì có tháng này")
+// }
+
+// const x = 12
+// // console.log (if (x > 9) console.log("x có hơn 2 chữ số")
+// // else console.log("x có 1 chữ số"))
+
+// console.log(x>9?console.log("x có hơn 2 chữ số"):console.log("x có 1 chữ số"))
+// // x>9?console.log("x có hơn 2 chữ số"):console.log("x có 1 chữ số")
+
+// if (true) {
+//     var x
+//     console.log("y")
+//     if (false ){
+
+//     }
+// } else {
+
+// }
+// console.log(3>2?"phép toán đúng": "phép toán sai")
+
+// for (let i = 1 ; i<= 5 ; i=i+1) {
+//     console.log(i)
+// }
+
+// i = "1"; true > console.log("1") > i = i + "1"
+//i ="11"; true ...
+
+// for (let i = 0; i <10; i++){
+//     console.log("tren",i)
+//     if (i==5) {
+//         continue
+//     }
+
+//     console.log("duoi",i)
+
+// }
+
+// let count = 0
+// let i = 1
+// while (count<963) {
+//     if (i%5==0){
+//         console.log(i)
+//         count++
+//     }
+//     i++
+// }
+// // i = 0; true >> 1 >> tăng i
+// // i = 1....
+
+
+// let i=0
+// do {
+//     console.log("do-w",i)
+//     i++
+// }
+// while (i<0)
+
+// i = 0
+// while (i<0){
+//     console.log("w",i)
+//     i++
+// }
+
+// for (let bang = 2; bang <=9 ; bang++){
+//     for (i = 1; i<=9 ;i++){
+//         console.log(`${bang}x${i}=${bang*i}`)
+//     }
+//     console.log("_____________________")
+// }
+
+// function tenHam() {
+//     console.log(1)
+//     console.log(2)
+//     console.log(3)
+// }
+
+// tenHam()
+
+// function giaiThua(x){
+//     let kq=1
+//     for (let i =1; i<=x;i++){
+//         kq = kq*i
+//     }
+//     console.log(kq)
+// }
+
+// giaiThua(6)
+
+// function tong(a,b,c){
+//     console.log(a+b+c)
+// }
+
+// function tong1(a,b) {
+//     console.log(a+b)
+//     return (a+b)
+// } 
+// // console.log(tong(6,2,4))
+// // console.log(tong1(6,2))
+
+// console.log(typeof tong1())
+
+// const x = confirm("bạn có trên 18 tuổi không")
+// if (x) {
+//     alert("bạn được vào trang web")
+// }
+// else {
+//     alert("bạn chưa đủ tuổi")
+// }
+
+// const x = prompt("tên bạn là gì")
+// console.log(x)
+// alert("xin chào " + x)
