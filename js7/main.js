7fb1b62207518fd854491bec53319d8877984edc
quanghuyav <huyav1999@gmail.com>
2023-07-27 21:59:45 +0700

    z

---
+++ js7/main.js
@@ -0,0 +1,42 @@
+// const contentElement = document.getElementById("content")
+
+// console.log(contentElement)
+
+// const itemElements = document.getElementsByClassName("item")
+
+// console.log(itemElements)
+
+// const h3Elements = document.getElementsByTagName("h3")
+
+// console.log(h3Elements)
+
+// const firstItemElement = document.querySelector(".item")
+
+// console.log(firstItemElement)
+// const container = document.querySelector(".container")
+// console.log(container)
+
+// const itemElements = container.querySelectorAll(".item")
+
+// console.log(itemElements)
+
+// 
+
+// const containerElement = document.querySelector(".container")
+
+// // containerElement.className = "container red"
+// // containerElement.title = "container"
+// // console.log(containerElement.className)
+
+// containerElement.setAttribute("hràestgef", "abdad")
+
+// console.log(containerElement.getAttribute("class"))
+
+const containerElement = document.querySelector(".container")
+
+const itemElement = document.querySelector(".item")
+
+containerElement.innerHTML = "<h1 class = 'heading'>hello<h1>"
+
+console.log(containerElement.innerHTML)
+
